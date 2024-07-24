b039a8e9a18c47f81064b3753329c94b896ec362
Bittu Kumar <bk7355583@gmail.com>
2024-07-25 01:17:45 +0530

    Cors implemented

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from "express"
 import { login, register } from "./controller/user.controler.js"
 import userRouter from "./route/user.route.js"
 import todo_route from "./route/todo.route.js"
+import cors from "cors"
 
 
 const app=express()
-app.use(express.json())
-
+app.use(cors())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.use("/api/v1/users",userRouter)
 
